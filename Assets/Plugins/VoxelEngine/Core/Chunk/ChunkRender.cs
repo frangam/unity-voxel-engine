cd3bd72c33f15338834bb4b16c436722f0d6d346
alfonso <alfonso.cueto@hotmail.es>
2013-06-27 14:02:07 +0200

    Core v0.3.6

---
+++ Assets/Plugins/VoxelEngine/Core/Chunk/ChunkRender.cs
@@ -0,0 +1,162 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ChunkRender {
+	
+	private static List<Vector3> verticiesMallaChunk;
+	private static List<int> triangulosMallaChunk;
+	private static List<Vector2> uvCoorTexturaMallaChunk;
+	private Dictionary<TipoBloque, SubMallaChunk> _subMallasChunk;
+	
+	
+	public static Mesh renderizar(Chunk chunk)
+	{
+		verticiesMallaChunk.Clear();
+		triangulosMallaChunk.Clear();
+		uvCoorTexturaMallaChunk.Clear();
+		_subMallasChunk.Clear();
+		Bloque bloque = null;
+		
+		for(int x=0; x < chunk.getNumBloquesEnX; x++)
+		{
+			for(int y=0; y < chunk.getNumBloquesEnY; y++)
+			{
+				for(int z=0; z < chunk.getNumBloquesEnZ; z++)
+				{
+					Vector3 posicionBloque = new Vector3(x,y,z);
+					//miramos los vecinos.
+					bloque = chunk[x, y, z];                    
+                    Bloque arriba = chunk[x, y + 1, z];
+					Bloque abajo = chunk[x, y - 1, z];
+					Bloque frente = chunk[x, y, z +1];
+					Bloque atras = chunk[x, y, z - 1];
+					Bloque izquierda = chunk[x + 1, y, z];
+					Bloque derecha = chunk[x - 1, y, z];
+					
+					//miramos si hace falta dibujar ese bloque.
+					if(bloque != null && bloque.esDibujable())
+					{
+						//hace falta dibujarlo!! miramos si ya existe su submalla en el diccionario.
+						if(_subMallasChunk.ContainsKey(bloque.getTipo()))
+						{
+							//existe asi que no hace falta crearlo.
+						}else{
+							//no existe asi que lo añadimos.
+							_subMallasChunk.Add(bloque.getTipo(), new SubMallaChunk(bloque.getTipo()));
+						}
+						
+						if(!arriba.esDibujable())
+						{
+							crearCaraMalla(bloque, posicionBloque, "arriba");
+						}
+						if(!abajo.esDibujable())
+						{
+							crearCaraMalla(bloque, posicionBloque, "abajo");
+						}
+						if(!frente.esDibujable())
+						{
+							crearCaraMalla(bloque, posicionBloque, "frente");
+						}
+						if(!atras.esDibujable())
+						{
+							crearCaraMalla(bloque, posicionBloque, "atras");
+						}
+						if(!izquierda.esDibujable())
+						{
+							crearCaraMalla(bloque, posicionBloque, "izquierda");
+						}
+						if(!derecha.esDibujable())
+						{
+							crearCaraMalla(bloque, posicionBloque, "derecha");
+						}
+					}
+				}
+			}
+		}
+		
+		/*
+		 *     mesh.vertices = verts;
+  	mesh.triangles = tri;
+    mesh.uv = uv;
+    mesh.subMeshCount = 3;  
+		 * */
+		Mesh mesh = new Mesh();
+		mesh.vertices = verticiesMallaChunk.ToArray();
+		mesh.uv = uvCoorTexturaMallaChunk.ToArray();
+		mesh.subMeshCount = _subMallasChunk.Count;
+		int i = 1;
+		foreach(KeyValuePair<TipoBloque,SubMallaChunk> smc in _subMallasChunk)
+		{
+	
+			mesh.SetTriangles(smc.Value.getTriangulos().ToArray(), i);
+			i++;
+		}
+		
+	}
+	
+	public void crearCaraMalla(Bloque bloque, Vector3 posicionBloque, string sitio)
+	{
+		int indeceVertices = verticiesMallaChunk.Count;
+		
+		switch(sitio){
+		case "arriba":
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,1,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,1,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,1,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,1,0));
+			break;
+			
+		case "abajo":
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,0,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,0,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,0,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,0,1));
+			break;
+			
+		case "frente":
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,0,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,0,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,1,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,1,1));
+			break;
+			
+		case "atras":
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,0,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,1,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,1,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,0,0));
+			break;
+			
+		case "izquierda":
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,0,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,1,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,1,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(1,0,1));
+			break;
+			
+		case "derecha":
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,0,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,1,1));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,1,0));
+			verticiesMallaChunk.Add(posicionBloque + new Vector3(0,0,0));
+			break;
+			
+		}
+		
+		uvCoorTexturaMallaChunk.Add(new Vector2(0, 0));
+		uvCoorTexturaMallaChunk.Add(new Vector2(1, 0));
+		uvCoorTexturaMallaChunk.Add(new Vector2(0, 1));
+		uvCoorTexturaMallaChunk.Add(new Vector2(1, 1));
+			
+			
+		List<int> tri = _subMallasChunk[bloque.getTipo()].getTriangulos();
+		tri.Add(indeceVertices);
+		tri.Add(indeceVertices+1);
+		tri.Add(indeceVertices+2);
+		
+		tri.Add(indeceVertices+2);
+		tri.Add(indeceVertices+3);
+		tri.Add(indeceVertices);
+	}
+	
+}
---
+++ Assets/Plugins/VoxelEngine/Core/Chunk/SubMallaChunk.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SubMallaChunk {
+
+	private List<int> triangulos;
+	private TipoBloque tipoBloqueARenderizar;
+	
+	public SubMallaChunk(TipoBloque tipoBloque)
+	{
+		triangulos = new List<int>();
+		tipoBloqueARenderizar = tipoBloque;
+	}
+	
+	public List<int> getTriangulos()
+	{
+		return triangulos;
+	}
+	
+	public TipoBloque getTipoBloqueARenderizar()
+	{
+		return tipoBloqueARenderizar;
+	}
+	public int getIndiceSubMalla(MallaChunk mallaChunk)
+	{
+		return MaterialesDelChunk.getIndiceMaterialSegunTipo(mallaChunk, tipoBloqueARenderizar);
+	}
+}
+	/// Este metodo sirve obtener el valor adecuado de una submalla de una Mesh cuando se utiliza el metodo Mesh.SetTriangles(int[] arrayTriangulosSubmalla, int indiceSubmalla).
+	/// Dicho indice sirve para indicar cual sera el material del MeshRenderer que se debe utilizar para renderizar esa submalla.
+	/// Hay que comprender cómo asigna Unity los materiales a las submallas de MeshRenderer:
+	/// >> La asignacion de materiales a una submalla de MeshRender se realiza teniendo en cuenta el indice de submalla que se le indica
+	/// >> y segun este, se accede al array de materiales de MeshRenderer orden inverso, es decir, desde el ultimo elemento hasta el primero.
+	/// >> Por ejemplo: indice de submalla 2 (indica que es la submalla numero 2). Suponiendo que el array de materiales tiene 4 elementos que están en las posiciones 0, 1, 2 y 3.
+	/// >> Pues bien, el material que se le asigna a la submalla es el material que ocupa la posicion 2 del array, porque la subllama 1 tendria asignado el ultimo elemento del array
+	/// >> de materiales el numero 3. La submalla 3 tendria asignado el material de la posicion del array 1, y la ultima cuarta submalla el elemento 0 del array.
+	/// 
+	/// 
