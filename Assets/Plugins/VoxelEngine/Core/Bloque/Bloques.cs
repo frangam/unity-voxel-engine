2530376b2bddfa5b1c903f3d0782fb899e1f5d15
Fran García <garmo@MacBook-Pro-de-Fran.local>
2013-07-03 18:20:17 +0200

    añado setters para modificar un bloque localizado en unas coordenadas del terreno especificas

--- Assets/Plugins/VoxelEngine/Core/Bloque/Bloques.cs
+++ Assets/Plugins/VoxelEngine/Core/Bloque/Bloques.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public static class Bloques{
+	#region getters de bloques segun coordenadas del terreno
 	/// <summary>
 	/// Obtiene el bloque localizado en las coordenadas del terreno especificadas
 	/// </summary>
@@ -17,8 +18,8 @@ public static class Bloques{
 	/// <param name='zTerren'>
 	/// Z terren.
 	/// </param>
-	public static Bloque getBloqueEnCoordsTerreno(int xTerreno, int yTerreno, int zTerren){
-		return getBloqueEnCoordsTerreno(new Vector3i());	
+	public static Bloque getBloqueEnCoordsTerreno(int xTerreno, int yTerreno, int zTerreno){
+		return getBloqueEnCoordsTerreno(new Vector3i(xTerreno, yTerreno, zTerreno));	
 	}
 	
 	/// <summary>
@@ -68,6 +69,61 @@ public static class Bloques{
 		
 		return bloque;
 	}
+	#endregion
+	
+	#region setters de bloques segun coordenadas del terreno
+	/// <summary>
+	/// Modifica el bloque localizado en las coordenadas del terreno especificadas con el bloque nuevo indicado
+	/// </summary>
+	/// <param name='bloque'>
+	/// Bloque.
+	/// </param>
+	/// <param name='xTerreno'>
+	/// X terreno.
+	/// </param>
+	/// <param name='yTerreno'>
+	/// Y terreno.
+	/// </param>
+	/// <param name='zTerreno'>
+	/// Z terreno.
+	/// </param>
+	public static void setBloqueEnCoordsTerreno(Bloque bloque, int xTerreno, int yTerreno, int zTerreno){
+		setBloqueEnCoordsTerreno(bloque, new Vector3i(xTerreno, yTerreno, zTerreno));
+	}
+	
+	/// <summary>
+	/// Modifica el bloque localizado en las coordenadas del terreno especificadas con el bloque nuevo indicado
+	/// </summary>
+	/// <param name='bloque'>
+	/// Bloque.
+	/// </param>
+	/// <param name='posicionEnCoordsTerreno'>
+	/// Posicion en coords terreno.
+	/// </param>
+	public static void setBloqueEnCoordsTerreno(Bloque bloque, Vector3i posicionEnCoordsTerreno){
+		// primero calculamos las coordenadas del chunk segun las coordenadas del Terreno
+		int xChunk = (posicionEnCoordsTerreno.x / Chunk.numBloquesEnX);
+		int yChunk = (posicionEnCoordsTerreno.y / Chunk.numBloquesEnY);
+		int zChunk = (posicionEnCoordsTerreno.z / Chunk.numBloquesEnZ);
+		
+        // comprobamos que las coordenadas del chunk no se salgan del rango
+        bool coordChunkFueraRango =  (xChunk < 0 || yChunk < 0 || zChunk < 0) || (xChunk >= Terreno.totalChunksX
+									|| yChunk >= Terreno.totalChunksY || zChunk >= Terreno.totalChunksZ);
+        //si las coordenadas del chunk no se salen de rango
+		if(!coordChunkFueraRango){
+			//calcula las coordenadas de bloque relativas al origen del chunk
+	        int xBloque = posicionEnCoordsTerreno.x % Chunk.numBloquesEnX;
+	        int yBloque = posicionEnCoordsTerreno.y % Chunk.numBloquesEnY;
+	        int zBloque = posicionEnCoordsTerreno.z % Chunk.numBloquesEnZ;
+			
+			//obtenemos el chunk del terreno
+			Chunk chunk = GameObject.Find("GeneradorTerreno").GetComponent<GeneradorTerreno>().terreno.getChunks()[xChunk, yChunk, zChunk];
+			
+			//modificamos el bloque con el nuevo bloque
+			chunk.setBloque(bloque, xBloque, yBloque, zBloque);
+		}
+	}
+	#endregion
 }
 
 
