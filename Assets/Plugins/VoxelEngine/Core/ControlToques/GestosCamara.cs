6a80ecee1570e513a0b1e80fb4d290b563ee33d8
alfonso <alfonso.cueto@hotmail.es>
2013-07-04 10:22:32 +0200

    Aumentada la sensibilidad del pan y el pinch

--- Assets/Plugins/VoxelEngine/Core/ControlToques/GestosCamara.cs
+++ Assets/Plugins/VoxelEngine/Core/ControlToques/GestosCamara.cs
@@ -8,20 +8,19 @@ public class GestosCamara : MonoBehaviour {
 		
 		var pan = new TKPanRecognizer();		
 		// when using in conjunction with a pinch or rotation recognizer setting the min touches to 2 smoothes movement greatly
-//		if( Application.platform == RuntimePlatform.IPhonePlayer )
-//			pan.minimumNumberOfTouches = 2;
+		if( Application.platform == RuntimePlatform.IPhonePlayer )
+			pan.minimumNumberOfTouches = 2;
 		
 		pan.gestureRecognizedEvent += ( r ) =>
 		{
-			float panX = pan.deltaTranslation.x / 100;
-			float panZ = pan.deltaTranslation.y / 100;
+			float panX = pan.deltaTranslation.x;
+			float panZ = pan.deltaTranslation.y;
 			Vector3 camPos = Camera.mainCamera.transform.position;
 			float newPosX = Mathf.Clamp(camPos.x + panX, 0, 40);
 			float newPosZ = Mathf.Clamp(camPos.z + panZ, 0, 40);
 
 			Camera.mainCamera.transform.position = new Vector3( newPosX, camPos.y, newPosZ);
 			//Debug.Log( "pan recognizer fired: " + r );
-
 		};
 		
 		// continuous gestures have a complete event so that we know when they are done recognizing
@@ -37,7 +36,8 @@ public class GestosCamara : MonoBehaviour {
 		var pinch = new TKPinchRecognizer();
 		pinch.gestureRecognizedEvent += ( r ) =>
 		{
-			Camera.mainCamera.fieldOfView = Mathf.Clamp(Camera.mainCamera.fieldOfView + pinch.deltaScale * 2, 40, 75);
+			Camera.mainCamera.fieldOfView = Mathf.Clamp(Camera.mainCamera.fieldOfView + pinch.deltaScale * 100, 40, 75);
+//			Debug.Log (pinch.deltaScale);
 //			Debug.Log( "pinch recognizer fired: " + r );
 		};
 		TouchKit.addGestureRecognizer( pinch );
